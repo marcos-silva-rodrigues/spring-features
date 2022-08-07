183012d2f8094641ffd2ac3f289490810d35fa15
marcos-silva-rodrigues <marcos.rodrigues.dev@gmail.com>
2022-08-07 11:17:50 -0300

    feat: init study

---
+++ src/main/java/com/spring/features/Spring/Features/SpringFeaturesApplication.java
@@ -0,0 +1,13 @@
+package com.spring.features.Spring.Features;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFeaturesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFeaturesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/features/Spring/Features/SpringFeaturesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.features.Spring.Features;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFeaturesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

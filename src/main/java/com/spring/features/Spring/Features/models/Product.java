47c29c04012310ca1e5478e886cc50bb13baa5a3
marcos-silva-rodrigues <marcos.rodrigues.dev@gmail.com>
2022-08-07 11:18:00 -0300

    feat: add model

---
+++ src/main/java/com/spring/features/Spring/Features/models/Product.java
@@ -0,0 +1,72 @@
+package com.spring.features.Spring.Features.models;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String name;
+
+    @Column
+    private Integer qtd;
+
+    @Column
+    private Date createdAt;
+
+    public Product() {
+    }
+
+    public Product(String name, Integer qtd) {
+        this.name = name;
+        this.qtd = qtd;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getQtd() {
+        return qtd;
+    }
+
+    public void setQtd(Integer qtd) {
+        this.qtd = qtd;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", qtd=" + qtd +
+                ", createAt=" + createdAt +
+                '}';
+    }
+}
